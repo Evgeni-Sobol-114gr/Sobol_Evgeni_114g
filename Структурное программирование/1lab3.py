5a87c97d301a3fe354e84938df998f08925578f8
AlexVinnik <alex.vinnik@list.ru>
2017-04-19 11:36:18 +0500

    Перенос

--- Структурное программирование/1lab3.py
+++
@@ -1,7 +0,0 @@
-x = int(input("Введите X:"))
-y = int(input("Введите Y:"))
-z = int(input("Введите Z:"))
-if (x > 80) or (y > 80) or (z > 80):
- print("Есть больше 80")
-else:
- print("Нет больше 80")
